cca2bc379d322cf29384adc6bf523c87cf24a52b
DevAkky <developerakky@gmaiil.com>
2023-02-28 01:04:09 +0530

    add to watchlist start

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -1,75 +1,86 @@
-import React, {useState} from 'react';
-import Box from '@mui/material/Box';
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
 import "./Login.css";
-import { TextField } from '@mui/material';
-import Button from '@mui/material/Button';
-import { toast  } from 'react-toastify';
-import { signInWithEmailAndPassword } from "firebase/auth"
-import { auth } from "../../firebase"
+import { TextField } from "@mui/material";
+import Button from "@mui/material/Button";
+import { toast } from "react-toastify";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
+import GoogleButton from "react-google-button";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const Login = ({ handleClose }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
- const [ email, setEmail ] = useState("")
- const [ password, setPassword ] = useState("")
+  const handleSubmit = async () => {
+    if (!email || !password) {
+      toast.warn("Please fill all the field", {
+        theme: "dark",
+      });
+    } else {
+      try {
+        const result = await signInWithEmailAndPassword(auth, email, password);
 
+        toast.success(`Log in Successfull. Welcome ${result.user.email}`, {
+          theme: "dark",
+        });
 
- const handleSubmit = async () => {
-
-       if(!email || !password){
-             toast.warn("Please fill all the field", {
-                    theme:"dark"
-               })
-                                           
-     } else{
-               try{
-                    const result =  await signInWithEmailAndPassword(
-                                            auth, 
-                                            email,
-                                            password)
-                                                                                                                    
-                  toast.success(`Log in Successfull. Welcome ${result.user.email}`, {
-                                            theme:"dark"
-                                            })
-
-                      handleClose()  // to close the modal
-                                    
-                 } catch(error){
-                      toast.error(error.message)                  
-                       }                                                                             
-            }         
+        handleClose(); // to close the modal
+      } catch (error) {
+        toast.error(error.message);
       }
+    }
+  };
+
+  const googleProvider = new GoogleAuthProvider();
+
+  const signinWithGoogle = () => {
+    signInWithPopup(auth, googleProvider).then((res) => {
+      toast.success(`Sign In Successfull. Welcome ${res.user.email}`, {
+        theme: "dark",
+      });
+    });
+  };
 
   return (
-     <>
-         <Box className="login-box" p={3}>
+    <>
+      <Box className="login-box" p={3}>
+        <TextField
+          label="Email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-<TextField  
- label="Email"
- type="email" 
- value={email}
- onChange={(e) => setEmail(e.target.value)}
- />
+        <TextField
+          label="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-<TextField  
- label="Password"
- type="password" 
- value={password}
- onChange={(e) => setPassword(e.target.value)}
- />
+        <Button
+          variant="contained"
+          style={{ backgroundColor: "rgb(7, 195, 237)" }}
+          onClick={handleSubmit}
+        >
+          LOG IN
+        </Button>
+      </Box>
 
+      {/* sign with google  */}
 
-<Button 
-variant="contained" 
-style={{ backgroundColor: "rgb(7, 195, 237)", fontWeight:"bold" }}
-onClick={handleSubmit}
-> 
-LOG IN
-</Button>
+      <Box className="google-login">
+        <span>OR</span>
 
-
-</Box>
-     </>
-  )
-}
+        <GoogleButton
+          style={{ width: "100%" }}
+          onClick={signinWithGoogle}
+        ></GoogleButton>
+      </Box>
+    </>
+  );
+};
 
 export default Login;
--- src/components/Authentication/UserSidebar.js
+++ src/components/Authentication/UserSidebar.js
@@ -1,139 +1,132 @@
-import React from 'react';
+import React from "react";
 import "./UserSidebar.css";
-import Drawer from '@mui/material/Drawer';
-import { UseCryptoValue } from "../../context/CryptoContext" 
-import { styled } from '@mui/material/styles';
-import Badge from '@mui/material/Badge';
-import Avatar from '@mui/material/Avatar';
-import Stack from '@mui/material/Stack';
-import { Button } from '@mui/material';
-import { signOut } from 'firebase/auth';
-import { auth } from '../../firebase';
-import { toast } from 'react-toastify';
-
-
+import Drawer from "@mui/material/Drawer";
+import { UseCryptoValue } from "../../context/CryptoContext";
+import { styled } from "@mui/material/styles";
+import Badge from "@mui/material/Badge";
+import Avatar from "@mui/material/Avatar";
+import Stack from "@mui/material/Stack";
+import { Button } from "@mui/material";
+import { signOut } from "firebase/auth";
+import { auth } from "../../firebase";
+import { toast } from "react-toastify";
 
 const StyledBadge = styled(Badge)(({ theme }) => ({
-'& .MuiBadge-badge': {
-  backgroundColor: '#44b700',
-  color: '#44b700',
-  boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
-  '&::after': {
-  position: 'absolute',
-  top: 0,
-  left: 0,
-  width: '100%',
-  height: '100%',
-  borderRadius: '50%',
-  animation: 'ripple 1.2s infinite ease-in-out',
-  border: '1px solid currentColor',
-  content: '""',
+  "& .MuiBadge-badge": {
+    backgroundColor: "#44b700",
+    color: "#44b700",
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    "&::after": {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      borderRadius: "50%",
+      animation: "ripple 1.2s infinite ease-in-out",
+      border: "1px solid currentColor",
+      content: '""',
+    },
   },
-},
-'@keyframes ripple': {
-  '0%': {
-  transform: 'scale(.8)',
-  opacity: 1,
+  "@keyframes ripple": {
+    "0%": {
+      transform: "scale(.8)",
+      opacity: 1,
+    },
+    "100%": {
+      transform: "scale(2.4)",
+      opacity: 0,
+    },
   },
-  '100%': {
-  transform: 'scale(2.4)',
-  opacity: 0,
-   },
-},
- }));
-
-
+}));
 
 const UserSidebar = () => {
+  const [state, setState] = React.useState({ right: false });
 
-const [state, setState] = React.useState({right: false,});
+  const { user } = UseCryptoValue();
 
-const { user } = UseCryptoValue()
-                       
-const toggleDrawer = (anchor, open) => (event) => {
-if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-                      return;
-}
-                                        
-setState({ ...state, [anchor]: open });
-};
+  console.log(user);
 
-const logout = () => {
-  signOut(auth)
-  toast.success("Logout Successfull", {
-      theme:"dark"
-  })
- toggleDrawer()
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
 
-}
+    setState({ ...state, [anchor]: open });
+  };
 
-return (
-     <>
-         {['right'].map((anchor) => (
-        <React.Fragment key={anchor}>
-          
+  const logout = () => {
+    signOut(auth);
+    toast.success("Logout Successfull", {
+      theme: "dark",
+    });
+    toggleDrawer();
+  };
+
+  return (
+    <>
+      {["right"].map((anchor) => (
+        <div key={anchor}>
           {/* avtar profile starts ------>> */}
 
-<Stack direction="row" spacing={2}>
-      <StyledBadge
-        overlap="circular"
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-        variant="dot"
-      >
-        <Avatar 
-        src={user.photoURL}  
-        alt={user.displayName || user.email}
-        onClick={toggleDrawer(anchor, true)}
-        className="avtar"
-        style={{ backgroundColor: "rgb(7, 195, 237)" }}
-        />
-
-      </StyledBadge>
-    </Stack>
+          <Stack direction="row" spacing={2}>
+            <StyledBadge
+              overlap="circular"
+              anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+              variant="dot"
+            >
+              <Avatar
+                src={user.photoURL}
+                alt={user.displayName || user.email}
+                onClick={toggleDrawer(anchor, true)}
+                className="avtar"
+                style={{ backgroundColor: "rgb(7, 195, 237)" }}
+              />
+            </StyledBadge>
+          </Stack>
 
           {/* avtar profile ends ------>> */}
-         
+
           <Drawer
             anchor={anchor}
             open={state[anchor]}
             onClose={toggleDrawer(anchor, false)}
           >
+            <div className="drawer-container">
+              <div className="propic">
+                <Avatar
+                  src={user.photoURL}
+                  alt={user.displayName || user.email}
+                  onClick={toggleDrawer(anchor, true)}
+                  className="avtar-pic"
+                  style={{ backgroundColor: "rgb(7, 195, 237)" }}
+                />
 
-          <div className='drawer-container'>
-                <div className='propic'>
-                  <Avatar 
-                    src={user.photoURL}  
-                    alt={user.displayName || user.email}
-                    onClick={toggleDrawer(anchor, true)}
-                    className="avtar-pic"
-                    style={{ backgroundColor: "rgb(7, 195, 237)" }}
-                   />
-
-                <span className='userdetail'>
-                      {user.displayName || user.email}
+                <span className="userdetail">
+                  {user.displayName || user.email}
                 </span>
 
-                <div className='watchlist'>
-                      <p>WatchList</p>
+                <div className="watchlist">
+                  <p>WatchList</p>
                 </div>
+              </div>
 
-                </div>
-
-                <Button 
-                variant="contained" 
+              <Button
+                variant="contained"
                 onClick={logout}
                 className="logout-btn"
-                >
+              >
                 Log Out
-                </Button>
-
-          </div>
-            
+              </Button>
+            </div>
           </Drawer>
-        </React.Fragment>
+        </div>
       ))}
-     </>
-  )
-}
+    </>
+  );
+};
 
 export default UserSidebar;
